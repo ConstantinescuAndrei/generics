10be9ea23bd3a7e22dc4bf37d07fc72a155ea94b
Andrei <constantinescu321@gmail.com>
2025-07-01 10:01:34 +0300

    add generic repository template

---
+++ Repositories/GenericRepository.cs
@@ -0,0 +1,8 @@
+namespace Generics.Repositories;
+
+public class GenericRepository
+{
+    // - Declare a generic list of items
+    // - Create a method to add a new item to the list
+    // - Create a method to retrieve all items from the list
+}
