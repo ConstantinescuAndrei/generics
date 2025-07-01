641881d23cd6073948168d6f21857ea78af3179e
Andrei <constantinescu321@gmail.com>
2025-07-01 23:08:14 +0300

    Add runtime error example

--- Program.cs
+++ Program.cs
@@ -2,3 +2,15 @@
 
 Console.WriteLine("Hello, World!");
 
+// ArrayList list = new ArrayList();
+//
+// list.Add("hello");
+// list.Add("world");
+// list.Add(15);
+//
+// foreach (var item in list)
+// {
+//     string newEl = (string)item;
+//     
+//     Console.WriteLine(newEl);
+// }
